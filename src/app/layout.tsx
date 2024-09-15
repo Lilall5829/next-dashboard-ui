91b3f22c6014b0eec0ffe185f9f54fdb306b5b2a
Lilall5829 <lilall5829@gmail.com>
2024-09-15 00:55:45 -0400

    set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// The layout for all of pages
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>Test{children}</body>
     </html>
   );
 }
