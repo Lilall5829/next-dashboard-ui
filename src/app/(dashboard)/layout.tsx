1a603f5b7e21218167e16c55a366ac734331e13a
Lilall5829 <lilall5829@gmail.com>
2024-09-29 15:25:46 -0400

    Navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -8,14 +11,18 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/*left*/}
-      <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center gap-2">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">School</span>
+          <span className="hidden lg:block"> School </span>
         </Link>
+        <Menu/>
       </div>
       {/*right*/}
-      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar/>
+      {children}
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="" width={14} height={14} />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Navbar;
