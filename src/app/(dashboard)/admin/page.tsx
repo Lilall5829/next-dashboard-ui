a1375895464c54a560ccbed324690036efce6ecf
Lilall5829 <lilall5829@gmail.com>
2024-12-02 13:50:58 -0500

    Update Tailwind style

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,10 @@
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+  );
+};
 
-export default Homepage
+export default Homepage;
